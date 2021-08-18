136d338b5e4b407b6b77d4db2bd6e76165860a20
Pawel Mendelski <mendelski.pawel@gmail.com>
2021-08-18 18:35:53 +0200

    Update dependencies and gh action

--- kotlin-junit5/build.gradle.kts
+++ kotlin-junit5/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    kotlin("jvm") version "1.4.10"
-    id("com.coditory.integration-test") version "1.1.10"
+    kotlin("jvm") version "1.5.21"
+    id("com.coditory.integration-test") version "1.2.3"
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
 
 tasks.withType<Test> {
