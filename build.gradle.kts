f653f0452ce07f970b2ba1d503d097ef408a9ebe
Paweł Mendelski <mendelski.pawel@gmail.com>
2024-12-07 10:35:46 +0100

    Fix folder names

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
-buildscript {
-    repositories {
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
-    }
-    dependencies {
-        classpath(files("/Users/mendlik/Code/gradle/gradle-integration-test-plugin/build/libs/integration-test-plugin-2.1.0.jar"))
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-    }
-}
-
-apply(plugin = "com.coditory.integration-test")
+//buildscript {
+//    repositories {
+//        maven {
+//            url = uri("https://plugins.gradle.org/m2/")
+//        }
+//    }
+//    dependencies {
+//        classpath(files("/Users/mendlik/Code/gradle/gradle-integration-test-plugin/build/libs/integration-test-plugin-2.1.0.jar"))
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+//    }
+//}
+//
+//apply(plugin = "com.coditory.integration-test")
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    // id("com.coditory.integration-test") version "2.1.0"
+    id("com.coditory.integration-test") version "2.2.0"
 }
 
 repositories {
@@ -24,15 +24,12 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.1")
+    integrationImplementation("com.coditory.quark:quark-context:0.1.22")
 }
 
-val customTask by tasks.registering {
-    println("Configuring customTask")
-    doLast {
-        println("Custom task executed")
-    }
-}
-
+// To test if config is lazy run:
+// ./gradlew clean
+// There should be no "Running test configuration...." in the logs
 tasks.withType<Test>().configureEach {
-    println("Long running configuration....")
+    println("Running test configuration....")
 }
